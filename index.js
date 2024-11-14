bfcde37fcd5a766edfef3adcbfe1b849e74a720d
kkwasny <kkwasny@student.42wolfsburg.de>
2024-11-14 16:48:07 +0100

    added ';' sign

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ let notes = [
 	response.json(note)
   })
 
-  const port = process.env.PORT || 4000
+  const port = process.env.PORT || 4000;
   app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
