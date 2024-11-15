5425fbc1291b6443ea49ec9d23d7853da81403fd
kkwasny <kkwasny@student.42wolfsburg.de>
2024-11-15 16:14:29 +0100

    changed the port

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ let notes = [
 	response.json(note)
   })
 
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3001;
   app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
